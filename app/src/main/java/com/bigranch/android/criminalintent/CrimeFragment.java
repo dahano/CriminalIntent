a845f14e6c9e992b148362b173f4142da4ea1956
ofir dahan <ofirdahan@live.com>
2017-02-01 21:48:17 -0800

    Added Contacts list functionality

--- app/src/main/java/com/bigranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bigranch/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,9 @@ package com.bigranch.android.criminalintent;
 import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.DialogFragment;
@@ -159,6 +162,12 @@ public class CrimeFragment extends Fragment{
             mSuspectButton.setText(mCrime.getSuspect());
         }
 
+        PackageManager packageManager = getActivity().getPackageManager();
+        if(packageManager.resolveActivity(pickContact,
+                PackageManager.MATCH_DEFAULT_ONLY)==null){
+            mSuspectButton.setEnabled(false);
+        }
+
         return v;
     }
 
@@ -172,13 +181,34 @@ public class CrimeFragment extends Fragment{
                     .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
             updateDate();
+        }else if (requestCode == REQUEST_CONTACT && data != null){
+            Uri contactUri = data.getData();
+            String[] queryFields = new String[] {
+                    ContactsContract.Contacts.DISPLAY_NAME
+            };
+            Cursor c = getActivity().getContentResolver()
+                    .query(contactUri, queryFields, null, null, null);
+            try{
+                if(c.getCount() == 0){
+                    return;
+                }
+
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                mCrime.setSuspect(suspect);
+                mSuspectButton.setText(suspect);
+            }finally {
+                c.close();
+            }
         }
+
         if (requestCode == REQUEST_TIME){
             Date date = (Date) data.getSerializableExtra(TimePickerFragment.EXTRA_TIME);
             mCrime.setDate(date);
             updateTime();
 
         }
+
     }
 
     private void updateDate() {
